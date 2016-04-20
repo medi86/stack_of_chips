f4a6813d431ff2e62ae20274ded279c609bc62e2
Dongmin Lee <medi8600@gmail.com>
2016-04-20 15:54:59 -0500

    Updating my solution

--- lib/chip.rb
+++ lib/chip.rb
@@ -1,1 +1,24 @@
 #write code here to make some tests pass
+class Chip
+  attr_reader :value
+  attr_accessor :color
+  include Comparable
+  def initialize(color, value)
+    @color =color
+    @value = value
+    @face = true
+  end
+
+  def face_up?
+    @face
+  end
+
+  def flip
+    face = [true, false]
+    @face = face.shuffle.shift
+  end
+
+  def <=>(other)
+    value <=> other.value
+  end
+end
